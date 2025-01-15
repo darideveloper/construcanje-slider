f19221a7ec960c4bae23c285bac47e39303a221c
daridev <darideveloper@gmail.com>
2025-01-15 12:13:40 -0600

    feat (swipper): remove auto play

--- script.js
+++ script.js
@@ -341,10 +341,6 @@ slides.forEach((slide) => {
 new Swiper(".swiper", {
   loop: true,
   speed: 1000,
-  autoplay: {
-    delay: 5000,
-    disableOnInteraction: true,
-  },
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
