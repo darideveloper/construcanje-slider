378f37a1e3923d45b680679657ae4c5fe526441e
daridev <darideveloper@gmail.com>
2025-01-27 12:05:12 -0600

    style: render 7 grid logos

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Slides data
 const imagesHost =
-  "https://raw.githubusercontent.com/SilviaEera/Swiper-Slider-wp/refs/heads/master/img";
+  "https://raw.githubusercontent.com/SilviaEera/Swiper-Slider-wp/refs/heads/master/img"
 // const imagesHost = "./img"
 let slides = [
   {
@@ -106,6 +106,10 @@ let slides = [
         name: "iBath",
         href: "https://ibath.com.ar/",
       },
+      {
+        name: "Deca",
+        href: 'https://aqualaf.com.ar/?gad_source=1&gbraid=0AAAAA9pSkZSeORxqf0DULa_32-XbQsAWc&gclid=CjwKCAiA-ty8BhA_EiwAkyoa3x1znQUisKtlb4gRevguTM-suYqwAGI7evTCNAFa2fKu6YLX5HBr3RoC-AkQAvD_BwE'
+      }
     ],
   },
   {
@@ -234,11 +238,12 @@ let slides = [
       },
     ],
   },
-];
+]
 
 // Calculate logos src and alt
 for (let slide of slides) {
   slide.logos = slide.logos.map((logo) => {
+    console.log(logo.name)
     return {
       src: `${imagesHost}/logos/logo-${logo.name
         .toLowerCase()
@@ -246,45 +251,46 @@ for (let slide of slides) {
       alt: "Logo de " + logo.name,
       href: logo.href,
       name: "Empresa asociada: " + logo.name,
-    };
-  });
+    }
+  })
 }
 
-const swiperWrapper = document.querySelector(".swiper-wrapper");
+const swiperWrapper = document.querySelector(".swiper-wrapper")
 
 // Setup all logos in slides
 slides.forEach((slide) => {
-  let logosHTML;
+  let logosHTML
 
-  if (slide.logos.length === 5) {
+  if (slide.logos.length % 2 == 1) {
     // It is tricky to handle 5 logo system.
-    const topRowLogos = slide.logos.slice(0, 3);
-    const bottomRowLogos = slide.logos.slice(3);
+    const logosMiddle = parseInt(slide.logos.length / 2)
+    const topRowLogos = slide.logos.slice(0, logosMiddle)
+    const bottomRowLogos = slide.logos.slice(logosMiddle)
 
     logosHTML = `
       <div class="top-row">
           ${topRowLogos
-            .map(
-              (logo) => `
+        .map(
+          (logo) => `
             <a class="logo-container" href="${logo.href}" target="_blank" title="${logo.name}">
                 <img src="${logo.src}" alt="${logo.alt}" class="logo" loading="lazy">
             </a>
           `
-            )
-            .join("")}
+        )
+        .join("")}
       </div>
       <div class="bottom-row">
           ${bottomRowLogos
-            .map(
-              (logo) => `
+        .map(
+          (logo) => `
             <a class="logo-container" href="${logo.href}" target="_blank" title="${logo.name}">
                 <img src="${logo.src}" alt="${logo.alt}" class="logo" loading="lazy">
             </a>
           `
-            )
-            .join("")}
+        )
+        .join("")}
       </div>
-  `;
+  `
   } else {
     // Normal handling for other logo counts
     logosHTML = slide.logos
@@ -295,12 +301,12 @@ slides.forEach((slide) => {
           </a>
         `
       )
-      .join("");
+      .join("")
   }
 
   const bgImageUrl = `${imagesHost}/bg/${slide.title
     .toLowerCase()
-    .replaceAll(" ", "-")}.webp`;
+    .replaceAll(" ", "-")}.webp`
   const slideHTML = `
     <div class="swiper-slide">
         <div class="slide-content">
@@ -315,9 +321,9 @@ slides.forEach((slide) => {
             </div>
         </div>
     </div>
-  `;
-  swiperWrapper.insertAdjacentHTML("beforeend", slideHTML);
-});
+  `
+  swiperWrapper.insertAdjacentHTML("beforeend", slideHTML)
+})
 
 // Start swipper
 new Swiper(".swiper", {
@@ -327,4 +333,4 @@ new Swiper(".swiper", {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
-});
+})
